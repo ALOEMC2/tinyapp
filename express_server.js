f76488ee3e4a8f7035122b437753317f371f3b98
Al OFlaherty <aloemc2@gmail.com>
2017-10-10 21:56:31 +0000

    random number generator

--- express_server.js
+++ express_server.js
@@ -1,13 +1,31 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+function generateRandomString() {
+return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
+
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
