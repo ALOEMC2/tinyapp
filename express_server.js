6b302c41add8901acfd84e9bf40a1f90cbae5650
Al OFlaherty <aloemc2@gmail.com>
2017-10-11 00:22:09 +0000

    day 2 completed

--- express_server.js
+++ express_server.js
@@ -11,20 +11,32 @@ return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
 }
 
 
-
-
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+
 };
+
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  let shortURL = req.params.shortURL
+  res.redirect(longURL);
+});
+
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let longURL = req.body.longURL;
+  console.log(longURL)
+  let shortURL =  generateRandomString()
+  urlDatabase[shortURL] = longURL;
+
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls", (req, res) => {
@@ -33,7 +45,16 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+
+
+  // TODO: Get longurl from urls database for a given short url
+  let longURL = urlDatabase[req.params.id]
+
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: longURL
+  };
+
   res.render("urls_show", templateVars);
 });
 
